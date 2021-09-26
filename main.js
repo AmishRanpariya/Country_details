0d4866ae329ad2f640f6a4cf4f98c6fd02ac788c
amishranpariya <amishranpariya@gmail.com>
2021-09-26 09:25:26 +0530

    api depreceted

--- main.js
+++ main.js
@@ -4,79 +4,83 @@ const filter_option_box = document.querySelector(".filter_options");
 
 const filter_options = document.querySelectorAll(".filter_options .option");
 
-
 filter_switch.addEventListener("click", () => {
-  filter_option_box.classList.toggle("hidden");
-})
-
-
-filter_options.forEach(item => {
-  item.addEventListener("click", () => {
-    if (item.innerText == "None") {
-      filter_switch.innerText = "Filter by Region";
-    }
-    else {
-      filter_switch.innerText = item.innerText;
-    }
-    filter_option_box.classList.toggle("hidden");
-    search_and_filter();
-  });
+	filter_option_box.classList.toggle("hidden");
 });
 
+filter_options.forEach((item) => {
+	item.addEventListener("click", () => {
+		if (item.innerText == "None") {
+			filter_switch.innerText = "Filter by Region";
+		} else {
+			filter_switch.innerText = item.innerText;
+		}
+		filter_option_box.classList.toggle("hidden");
+		search_and_filter();
+	});
+});
 
 const container = document.querySelector("#country_data_container");
 const search_input = document.querySelector("#search_input");
 search_input.addEventListener("input", (e) => {
-  e.preventDefault();
-  search_and_filter();
+	e.preventDefault();
+	search_and_filter();
 });
 
-
 function search_and_filter() {
-  if (filter_switch.innerText == "Filter by Region") {
-    renderData(search_input.value);
-  } else {
-    renderData(search_input.value, filter_switch.innerText);
-  }
+	if (filter_switch.innerText == "Filter by Region") {
+		renderData(search_input.value);
+	} else {
+		renderData(search_input.value, filter_switch.innerText);
+	}
 }
 
-
 let data = JSON.parse(localStorage.getItem("REST_Country_api_data")) || "";
-
+/*
+*
+*
+*
+//depreceted
+*
+*
+*
+*/
 if (data == "") {
-  fetch("https://restcountries.eu/rest/v2/all")
-    .then((res) => {
-      return res.json();
-    })
-    .then((res) => {
-      data = res;
-      localStorage.setItem("REST_Country_api_data", JSON.stringify(data));
-      renderData();
-    })
-    .catch(err => {
-      console.log(err);
-      container.innerHTML = "<h3>NO DATA FOUND</h3>";
-    });
+	fetch("https://restcountries.eu/rest/v2/all")
+		.then((res) => {
+			return res.json();
+		})
+		.then((res) => {
+			data = res;
+			localStorage.setItem("REST_Country_api_data", JSON.stringify(data));
+			renderData();
+		})
+		.catch((err) => {
+			console.log(err);
+			container.innerHTML = "<h3>NO DATA FOUND</h3>";
+		});
 } else {
-  renderData();
+	renderData();
 }
 
 function renderData(term = "", term2 = "") {
+	container.innerHTML = "";
 
-  container.innerHTML = "";
+	tempdata = data.filter((item) => {
+		item = Object.values(item);
+		item = JSON.stringify(item).toLocaleLowerCase();
+		return (
+			item.includes(term.toLocaleLowerCase()) &&
+			item.includes(term2.toLocaleLowerCase())
+		);
+	});
 
-  tempdata = data.filter((item) => {
-    item = Object.values(item);
-    item = JSON.stringify(item).toLocaleLowerCase();
-    return (item.includes(term.toLocaleLowerCase()) && item.includes(term2.toLocaleLowerCase()));
-  });
-
-
-  tempdata.forEach((item, index) => {
-    if (index < 20) {
-
-      container.innerHTML += `
-        <a class="card" href="https://amishranpariya.github.io/Country_details/About/country.html?cc=${item.alpha3Code}">
+	tempdata.forEach((item, index) => {
+		if (index < 20) {
+			container.innerHTML += `
+        <a class="card" href="https://amishranpariya.github.io/Country_details/About/country.html?cc=${
+					item.alpha3Code
+				}">
         <div class="upper">
             <img src="${item.flag}" alt="flag">
         </div>
@@ -84,7 +88,9 @@ function renderData(term = "", term2 = "") {
             <h3>${item.name}</h3>
             <p>
             <span class="label" >Population: </span>
-            <span class="label_value" >${(item.population).toLocaleString('en-US')}</span>
+            <span class="label_value" >${item.population.toLocaleString(
+							"en-US"
+						)}</span>
             </p>
             <p>
             <span class="label" >Region: </span>
@@ -97,8 +103,6 @@ function renderData(term = "", term2 = "") {
         </div>
         </a>
           `;
-    }
-  });
-
-
+		}
+	});
 }
